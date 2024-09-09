84fe0f0c19148750c3fb02741e662c13f1e29bf5
exoscar <76253095+exoscar@users.noreply.github.com>
2024-09-09 12:20:38 +0530

    demo code

---
+++ src/components/screens/DemoCode/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "./style";
+import { Typography } from "@mui/material";
+
+const DemoPage = () => {
+  return (
+    <Container>
+      <Typography className="some-text">Apples are Sweet</Typography>
+    </Container>
+  );
+};
+
+export default DemoPage;
---
+++ src/components/screens/DemoCode/style.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: hotpink;
+
+  .some-text {
+    font-size: 24px;
+    font-weight: bold;
+    color: #333;
+    background-color: #f0f0f0;
+  }
+`;
